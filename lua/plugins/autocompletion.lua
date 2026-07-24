2285ab7ce2870e49135b4209a3e1a5af98661e42
brunodavid <brunodavidlisanti@gmail.com>
2026-07-23 22:51:54 -0300

    Aceptar seleccion de autocompletado con tab

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -11,11 +11,20 @@ for server, opts in pairs(servers) do
   vim.lsp.config(server, opts)
 end
 
--- Config: Blink Completado
 require('blink.cmp').setup({
-  keymap = { preset = 'enter' },
+	keymap = {
+    preset = 'super-tab',
+    -- -- Navegar hacia abajo en la lista
+    -- ['<C-n>'] = { 'select_next', 'fallback' },
+    -- -- Navegar hacia arriba en la lista (recomendado)
+    -- ['<C-p>'] = { 'select_prev', 'fallback' },
+    -- -- Aceptar la sugerencia. El 'fallback' asegura que si el menú 
+    -- -- está cerrado, la tecla Tab funcione normalmente en tu código.
+    -- ['<Tab>'] = { 'accept', 'fallback' },
+    -- ['<CR>'] = { 'fallback' }
+  },
   appearance = { nerd_font_variant = 'mono' },
-  completion = { documentation = { auto_show = false, auto_show_delay_ms = 500 } },
+  completion = { documentation = { auto_show = false, auto_show_delay_ms = 0 } },
   sources = {
     default = { 'lsp', 'path' },
   },
